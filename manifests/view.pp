76662dd0d0aac22ec6628d06c2c4ae401af6fd08
Simon Fowler <simon.fowler@anu.edu.au>
2016-01-28 20:36:19 +1100

    Reporting puppet module - initial implementation.
    This has received light testing and appears to work.
    
    The puppet module depends on the apache and apt modules, and can be used
    by simply including the reporting::(api|pollster|view) modules.
    Configuration is entirely in hiera, with (at present) the following
    values:
    
    Change-Id: I8cdc29a3adc38b6ac5a35be1b210a8490fe7a490
    reporting::api_host: "localhost"
    reporting::api_port: 9494
    reporting::api::api_host: "localhost"
    reporting::api::api_port: 9494
    reporting::api::db_host: "localhost"
    reporting::api::db_port: 3306
    reporting::api::db_user: 'reporting-test'
    reporting::api::db_pass: 'Testing out the system'
    reporting::api::db_name: 'reporting'
    reporting::api::keystone_auth_url: ""
    reporting::api::keystone_identity_url: ""
    reporting::api::admin_user: ""
    reporting::api::admin_pass: ""
    reporting::api::admin_tenant: ""
    reporting::api::auth_role: ""
    reporting::pollster::db_host: 'localhost'
    reporting::pollster::db_port: 3306
    reporting::pollster::db_user: 'reporting-test'
    reporting::pollster::db_pass: 'Testing out the system'
    reporting::pollster::db_name: 'reporting'
    reporting::pollster::source_db_user: 'reporting-test'
    reporting::pollster::source_db_pass: 'Testing out the system'
    reporting::pollster::source_db_name: 'reporting'
    reporting::pollster::source_db_host: 'localhost'
    reporting::pollster::keystone_auth_url: ""
    reporting::pollster::nova_user: ""
    reporting::pollster::nova_pass: ""
    reporting::pollster::pollster_tenant: ""
    reporting::view::host: "localhost"
    reporting::view::path: ""
    reporting::view::admin_email: "root@localhost"
    reporting::view::api_host: ""
    reporting::view::api_port: ""

---
+++ manifests/view.pp
@@ -0,0 +1,44 @@
+# Reporting view layer server install and configuration
+class reporting::view (
+  $host='localhost',
+  $path='',
+  $admin_email='root@localhost',
+  $api_host,
+  $api_port
+) inherits reporting {
+  package {'reporting-view':
+    ensure => installed,
+  }
+
+  class { 'apache::mod::wsgi': }
+
+  file {'/etc/reporting-view/config.js':
+    owner   => root,
+    group   => root,
+    mode    => '0640',
+    content => template('reporting/reporting-view/config.js.erb'),
+    require => Package['reporting-view'],
+  }
+
+  apache::vhost {$host:
+    serveradmin         => $admin_email,
+    port                => 80,
+    docroot             => '/var/lib/www',
+    wsgi_script_aliases => { '/' => '/usr/share/reporting-view/reporting_view.py' },
+    aliases             => [
+      {
+        aliasmatch  => '/static',
+        path        => '/usr/share/reporting-view/static',
+      },
+    ],
+    directories         => [
+      {
+        path => '/usr/share/reporting-view'
+      },
+      {
+        path    => '/usr/share/reporting-view/static',
+        options => ['-Indexes']
+      },
+    ],
+  }
+}
