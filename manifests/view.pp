f00436cf27b1eb25d5e0d42837fa08b844bd804e
Simon Fowler <simon.fowler@anu.edu.au>
2016-04-19 17:06:04 +1000

    Add configuration to handle running behind an LB.
    Mostly this just means allowing more fine-grained configuration of the
    URL that people see and the apache configuration behind it.
    
    Change-Id: Icb46459885dd4aec599778bc407e9cd8617cb772

--- manifests/view.pp
+++ manifests/view.pp
@@ -1,6 +1,18 @@
 # Reporting view layer server install and configuration
+#
+# Note: port and external_port are split up this way because part of the
+# config we're working with is internal (the apache config) and part of it is
+# external (the stuff the browser deals with). Sometimes those two will have
+# a mediating agent in between (a load balancer, most likely) and there's no
+# guarantee that it'll be listening on the same port. So, we need to split the
+# external port out into its own variable, and we likewise need to split the
+# protocol part of the URL out (since the external mediator may be doing TLS
+# termination).
 class reporting::view (
   $host='localhost',
+  $port=80,
+  $external_port=80,
+  $proto='http',
   $path='',
   $admin_email='root@localhost',
   $api_host,
@@ -35,7 +47,7 @@ class reporting::view (
 
   apache::vhost {$host:
     serveradmin         => $admin_email,
-    port                => 80,
+    port                => $port,
     docroot             => '/var/lib/www',
     wsgi_script_aliases => { '/' => '/usr/share/reporting-view/reporting_view.py' },
     aliases             => [
